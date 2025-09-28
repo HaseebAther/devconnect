dcc8c0cf2edcbff22d6a37477d028ac41798ac5f
Hassaan Yousaf <Hassanyousaf828@gmail.com>
2025-09-28 16:03:54 +0500

    push to file

--- backend/users/views.py
+++ backend/users/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework.serializers import ModelSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 # Serializer for user registration
 class UserSerializer(ModelSerializer):
@@ -24,3 +26,18 @@ class UserSerializer(ModelSerializer):
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class logintokenSerializer(TokenObtainPairView):
+    def validate(self, attrs):
+        data= super.validate(attrs)
+
+        data['user']=
+        {
+            'id': self.User.id
+            'username': self.User.username
+            'email': self.User.email
+        }
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = logintokenSerializer       
